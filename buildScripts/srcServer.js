a94d5beeab2acda8d9133c14aeb7d84f51b7b3f9
dmawardi <dmawardi@gmail.com>
2020-05-05 22:56:18 +1000

    Webpack and CSS

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,14 +1,30 @@
 import express from "express";
 import path from "path";
 import open from "open";
+// Import webpack and config file
+import webpack from "webpack";
+import config from "../webpack.config.dev";
 
+// Express setup
 const port = 3000;
 const app = express();
 
+// Webpack
+const compiler = webpack(config);
+// Integrate webpack into Express
+app.use(
+	require("webpack-dev-middleware")(compiler, {
+		noInfo: true,
+		publicPath: config.output.publicPath,
+	})
+);
+
+// Routes
 app.get("/", function (req, res) {
 	res.sendFile(path.join(__dirname, "../src/index.html"));
 });
 
+// Run Server and listen to port
 app.listen(port, function (err) {
 	if (err) {
 		console.log(err);
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,18 +1,26 @@
 import path from "path";
 
 export default {
+	// Enable debug info
 	debug: true,
+
 	devtool: "inline-source-map",
+	// Display info
 	noInfo: false,
+	// Good place to inject middleware for hot reloading
 	entry: [path.resolve(__dirname, "src/index")],
+	// Web, Node or Electron
 	target: "web",
+	// This doesn't generate physical files. Served from memory
 	output: {
 		path: path.resolve(__dirname, "src"),
 		publicPath: "/",
 		filename: "bundle.js",
 	},
+	// Can add Webpack plugins here
 	plugins: [],
 	module: {
+		// Add all types of files here and they will be bundled together
 		loaders: [
 			{ test: /\.js$/, exclude: /node_modules/, loaders: ["babel"] },
 			{ test: /\.css$/, loaders: ["style", "css"] },
